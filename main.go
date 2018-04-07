a673cd8539d27125c4179627e299b631b17bec60
Evelyn3333 <misscookiess@gmail.com>
2018-04-07 15:55:29 -0700

    Update Handler post and Add GCS

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 	"reflect"
 	"github.com/pborman/uuid"
 	"context"
-	"cloud.google.com/go/bigtable"
+	"cloud.google.com/go/storage"
+	//"cloud.google.com/go/bigtable"
+	"io"
+
 )
 
 type Location struct {
@@ -23,7 +26,20 @@ type Post struct {
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
+	Url    string `json:"url"`
 }
+
+const (
+	INDEX    = "around"
+	TYPE     = "post"
+	DISTANCE = "200km"
+	// Needs to update
+	//PROJECT_ID = "the-option-199021"
+	//BT_INSTANCE = "around-post"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.224.174.212:9200" //////////////////////////////////每次启动要改 IP 地址
+	BUCKET_NAME = "post-images-199021"
+)
 
 func main() {
 	// Create a client
@@ -63,45 +79,6 @@ func main() {
 	http.HandleFunc("/search", handlerSearch)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-func handlerPost(w http.ResponseWriter, r *http.Request) {
-	// Parse from body of request to get a json object.
-	fmt.Println("Received one post request")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
-		return
-	}
-	id := uuid.New()
-	// Save to ES.
-	saveToES(&p, id)
-	fmt.Printf("Post is saved to Index: %s\n", p.Message)
-
-	ctx := context.Background()
-	//update project name here
-	bt_client, err := bigtable.NewClient(ctx, "the-option-199021", "around-post")
-	if err != nil {
-		panic(err)
-		return
-	}
-
-	tbl := bt_client.Open("post")
-	mut := bigtable.NewMutation()
-	t := bigtable.Now()
-
-	mut.Set("post", "user", t, []byte(p.User))
-	mut.Set("post", "message", t, []byte(p.Message))
-	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
-	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
-
-	err = tbl.Apply(ctx, id, mut)
-	if err != nil {
-		panic(err)
-		return
-	}
-	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
-
-}
 
 // Save a post to ElasticSearch
 func saveToES(p *Post, id string) {
@@ -128,16 +105,33 @@ func saveToES(p *Post, id string) {
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
 }
 
-const (
-	INDEX    = "around"
-	TYPE     = "post"
-	DISTANCE = "200km"
-	// Needs to update
-	PROJECT_ID = "the-option-199021"
-	BT_INSTANCE = "around-post"
-	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.188.135.203:9200" //////////////////////////////////每次启动要改 IP 地址
-)
+
+func saveToGCS(ctx context.Context, r io.Reader, bucket, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+	bh := client.Bucket(bucket)
+	if _ , err =  bh.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+	obj := bh.Object(name)
+	w := obj.NewWriter(ctx)
+	if _, err = io.Copy(w, r); err != nil {
+		return nil, nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, nil, err
+	}
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+	attrs, err := obj.Attrs(ctx)
+	//return to console
+	fmt.Printf("Post is saved to GCS : %s \n", attrs.MediaLink)
+	return obj, attrs, err
+}
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one request for search")
@@ -204,34 +198,62 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
-//func handlerSearch(w http.ResponseWriter, r *http.Request) {
-//	fmt.Println("Received one request for search")
-//	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
-//	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
-//	// range is optional
-//	ran := DISTANCE
-//	if val := r.URL.Query().Get("range"); val != "" {
-//		ran = val + "km"
-//	}
-//
-//	fmt.Println("range is ", ran)
-//
-//	// Return a fake post
-//	p := &Post{
-//		User:"1111",
-//		Message:"一生必去的100个地方",
-//		Location: Location{
-//			Lat:lat,
-//			Lon:lon,
-//		},
-//	}
-//
-//	js, err := json.Marshal(p)
-//	if err != nil {
-//		panic(err)
-//		return
-//	}
-//
-//	w.Header().Set("Content-Type", "application/json")
-//	w.Write(js)
-//}
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Other codes
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+	r.ParseMultipartForm(32 << 20)
+
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+	p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+
+	// replace it with your real bucket name.
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		return
+	}
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
+	// Save to ES.
+	saveToES(p, id)
+
+	// Save to BigTable.
+	//saveToBigTable(p, id)
+}
+
+
+
+
+
